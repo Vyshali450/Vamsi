ef8cc8274371b450d735a1d9481295cb39da2920
vamsichowdary <vamsichowdary@localhost>
2019-11-26 12:57:08 -0500

    my first commit

---
+++ November/src/main/java/learning/November/Newtesting.java
@@ -0,0 +1,11 @@
+package learning.November;
+
+import org.testng.annotations.Test;
+
+public class Newtesting {
+	@Test
+	public void mobiletesting() {
+		System.out.println("vamsi this my maven");
+	}
+
+}
